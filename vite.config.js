efbbfd4dc51216a7304c7703c81342853f4590a6
Karl Keshavarzi <kkeshavarzi@outlook.com>
2025-12-17 15:34:20 -0800

    test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/karl-keshavarzi/",
+  base: "/",
   plugins: [react()],
 });
